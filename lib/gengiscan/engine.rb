fd1d40078aeb6c0959e2ce212e0f421156781f22
Paolo Perego <thesp0nge@gmail.com>
2012-09-06 15:05:31 +0200

    Added a check for phpinfo

--- lib/gengiscan/engine.rb
+++ lib/gengiscan/engine.rb
@@ -6,19 +6,22 @@ module Gengiscan
 
 
     attr_reader :res
+    attr_reader :body
 
 
 
     def initialize      
     end
 
+    
     def detect(url)
       uri = URI(url)
       begin 
         res = Net::HTTP.get_response(uri)
-        {:status=>:OK, :code=>res.code, :server=>res['Server'], :powered=>res['X-Powered-By'], :generator=>get_generator_signature(res)} 
-      rescue
-        {:status=>:KO, :code=>nil, :server=>nil, :powered=>nil, :generator=>nil}
+        body = check_for_phpinfo(uri)
+        {:status=>:OK, :message=> nil, :code=>res.code, :server=>res['Server'], :powered=>res['X-Powered-By'], :generator=>get_generator_signature(res), :php_info=>! body.empty?} 
+      rescue => e
+        {:status=>:KO, :message=>e.message, :code=>nil, :server=>nil, :powered=>nil, :generator=>nil, :php_info=>nil}
       end
     end
 
@@ -33,6 +36,24 @@ module Gengiscan
 
       generator
     end
+
+    def check_for_phpinfo(uri)
+      body = ""
+      r = Net::HTTP.get_response(URI("#{uri.scheme}://#{uri.host}:#{uri.port}/phpinfo.php"))
+      filename = "#{uri.host}_phpinfo.html"
+      body = r.body if r.code == "200"
+      r = Net::HTTP.get_response(URI("#{uri.scheme}://#{uri.host}:#{uri.port}/info.php")) if body.nil?
+      filename = "#{uri.host}_info.html" if body.nil?
+      body = r.body if r.code == "200" 
+
+      f= File.new(filename, "w")
+      f.puts(body)
+      f.close
+
+
+      body
+    end
+
 
   end
 end
